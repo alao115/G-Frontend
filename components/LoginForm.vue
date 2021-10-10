8c29cdad7b77cde91b97040bf87cabc3f06f7fa6
Hercege <ronald.guedegbe@gmail.com>
2021-10-10 20:22:58 +0100

    initial commit

---
+++ components/LoginForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex space-x-8 justify-center">
+    <div class="container flex flex-col w-5/12 justify-between" style="min-height: 516px">
+      <GontcheLogo />
+      <div class="text-white">
+        <h1 class="text-5xl font-bold">
+          Prêt pour une nouvelle aventure
+        </h1>
+        <p class="text-3xl mt-6">
+          Un logement pour tous.
+        </p>
+      </div>
+      <div class="copyright text-white mt-16 -mb-12">
+        <p>
+          © 2021 Gontché
+        </p>
+        <div class="flex space-x-3">
+          <NuxtLink to="#">
+            <span class="icon">
+              <i class="la la-facebook-f text-2xl" />
+            </span>
+          </NuxtLink>
+          <NuxtLink to="#">
+            <span class="icon">
+              <i class="la la-twitter text-3xl" />
+            </span>
+          </NuxtLink>
+          <NuxtLink to="#">
+            <span class="icon">
+              <i class="la la-instagram text-3xl" />
+            </span>
+          </NuxtLink>
+          <NuxtLink to="#">
+            <span class="icon">
+              <i class="la la-linkedin-square text-3xl" />
+            </span>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+    <div class="container flex-col bg-white w-5/12 border-t-4 border-blue-730 flex justify-center item-center pb-12 px-16" style="min-height: 516px">
+      <div class="w-full">
+        <p class="title font-semibold text-3xl mt-32 mb-6 text-sky-450">
+          Connexion
+        </p>
+        <form action="#" class="pt-4">
+          <input type="text" class="h-20 px-8 mt-1 mb-4 block w-full border-gray-320 focus:border-sky-450 rounded-md bg-gray-100 focus:bg-white focus:ring-0 placeholder-gray-600 focus:placeholder-blue-380" placeholder="Email">
+          <input type="password" class="h-20 px-8 mb-4 mt-1 block w-full border-gray-320 focus:border-sky-450 rounded-md bg-gray-100 focus:bg-white focus:ring-0 placeholder-gray-600 focus:placeholder-blue-380" placeholder="Mot de passe">
+          <p class="text-sm mb-12 text-gray-400">
+            <NuxtLink to="/new-password" class="text-gray-400 hover:text-blue-920 font-semibold">
+              Mot de passe oublié
+            </NuxtLink>
+          </p>
+          <div class="rounded-md shadow w-2/3 mb-8">
+            <NuxtLink to="/directory" class="shadow-btn-shadow w-full flex items-center justify-center px-8 h-14 border border-transparent text-base font-medium rounded-md text-white bg-sky-550 hover:bg-blue-920 md:py-4 md:text-lg md:px-10">
+              Se connecter
+            </NuxtLink>
+          </div>
+        </form>
+        <p class="text-sm my-12 text-blue-920">
+          Vous n'avez pas encore de compte ?
+          <NuxtLink to="/new-password" class="text-blue-300 hover:text-blue-920 font-semibold">
+            Cliquez ici
+          </NuxtLink>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/TheDashboardSidenav.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="flex flex-col items-center h-full overflow-hidden text-gray-700 rounded border-r-2 border-gray-200 flex-none" :class="isMinified === true ? 'w-16' : 'w-56'">
+    <div class="w-full px-2">
+      <a class="flex items-center w-full h-16" href="#" @click.prevent="isMinified = !isMinified">
+        <img src="/assets/images/logo.png" alt="" class="logo">
+        <span class="ml-2 text-sm font-bold" :class="isMinified === true ? 'hidden' : ''">Annuaire du DM</span>
+      </a>
+    </div>
+    <div class="w-full px-2">
+      <div class="flex flex-col items-center w-full mt-2 py-2 border-t border-gray-300">
+        <a class="flex items-center w-full h-12 px-3 my-2 text-white bg-blue-730 rounded" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+          </svg>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Ajouter un membre</span>
+        </a>
+      </div>
+      <div class="flex flex-col items-center w-full border-t border-gray-300">
+        <NuxtLink to="/directory" class="flex items-center w-full h-12 px-3 my-2 rounded text-blue-730 bg-blue-75">
+          <span class="icon w-6 block">
+            <i class="far fa-book-open mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Annuaire</span>
+        </NuxtLink>
+      </div>
+      <div class="flex flex-col items-center w-full border-t border-gray-300">
+        <NuxtLink to="/districts" class="flex items-center w-full h-12 px-3 my-2 rounded hover:bg-gray-200">
+          <span class="icon w-6 block">
+            <i class="far fa-square mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Districts</span>
+        </NuxtLink>
+        <NuxtLink to="/countries" class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-200">
+          <span class="icon w-6 block">
+            <i class="far fa-flag mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Pays</span>
+        </NuxtLink>
+        <NuxtLink to="/regions" class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-200">
+          <span class="icon w-6 block">
+            <i class="far fa-th-large mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Régions</span>
+        </NuxtLink>
+        <NuxtLink to="/zones" class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-200">
+          <span class="icon w-6 block">
+            <i class="far fa-th mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Zones</span>
+        </NuxtLink>
+        <NuxtLink to="/clubs" class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-200">
+          <span class="icon w-6 block">
+            <i class="far fa-users mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Clubs</span>
+        </NuxtLink>
+      </div>
+      <div class="flex flex-col items-center w-full mt-2 border-t border-gray-300">
+        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-200" href="#">
+          <span class="icon w-6 block">
+            <i class="far fa-cog mx-auto block"></i>
+          </span>
+          <span class="ml-3 text-sm font-medium" :class="isMinified === true ? 'hidden' : ''">Réglages</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedUserDropdownnClosed: true,
+      isMinified: false,
+      isDismissed: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 48px;
+}
+</style>
