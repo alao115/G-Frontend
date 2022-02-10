f88c602e5b686ba5650475eed7f2d0bf331fcc86
Hercege <ronald.guedegbe@gmail.com>
2022-02-10 06:39:16 +0100

    last css test to correct input border style

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      /* { rel: 'stylesheet', href: '/assets/css/main.css' }, */
+      { rel: 'stylesheet', href: '/assets/css/main.css' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
