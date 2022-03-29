6f06a5ea38f6a79d5a287250656ff2b339842dcd
Hercege <ronald.guedegbe@gmail.com>
2022-03-29 11:07:48 +0100

    modifs

--- components/signup/ActivationSuccessful.vue
+++ components/signup/ActivationSuccessful.vue
@@ -3,8 +3,11 @@
     <div class="w-full">
       <img src="/assets/images/success.svg" alt="Logo Long Gontché" class="success-img mx-auto">
       <p class="text-sm my-12 text-blue-920">
-        Veuillez consulter votre boite mail et cliquer le lien qui vous a été transmis pour valider votre compte.
+        Votre compte a été bien activé. Merci !
       </p>
+      <button type="button" class="shadow-btn-shadow border border-transparent py-4 text-sm px-8 leading-none rounded font-medium mt-8 text-white bg-sky-550 hover:bg-blue-920">
+        Connexion
+      </button>
     </div>
   </div>
 </template>
