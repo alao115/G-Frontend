3ff7f29fc3e6f82cae4065b8fb0119f31023f20d
Ronald GUEDEGBE <rguedegbe@sfp-groupe.com>
2022-04-26 08:33:47 +0100

    modifs mineures

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="font-body bg-contain bg-center h-screen w-screen pt-32 overflow-x-hidden flex items-center">
-    <div class="container w-full px-8 2xl:px-16 xl:px-24 flex flex-col justify-center mx-auto" style="min-height: 516px">
+    <div class="container w-full px-8 2xl:px-16 xl:px-24 mx-auto" style="min-height: 516px">
       <h3>Résultats de la recherche</h3>
       <div class="flex">
         <div class="filters w-1/4">
