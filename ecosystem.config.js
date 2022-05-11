35deca8db895eba745d11770a07d8a29fa528bc3
alao115 <chegunmoulero@gmail.com>
2022-05-11 02:40:58 +0100

    pm2 config file updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  apps : [
-	{
-    script: '',
-    watch: '.'
-  }
-  ],
-};
+  apps: [
+    {
+      name: 'gontche_frontend_prod',
+      script: 'npm run start',
+      watch: ['nuxt-dist']
+    },
+    {
+      name: 'gontche_frontend_dev',
+      script: 'npm run dev',
+      watch: ['nuxt-dist']
+    }
+  ]
+}
