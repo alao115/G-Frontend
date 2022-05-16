dcb9f075594bafb1c5b057037a679823c7dde06a
alao115 <chegunmoulero@gmail.com>
2022-05-16 13:29:51 +0100

    updates

--- plugins/services/publication.service.js
+++ plugins/services/publication.service.js
@@ -1,5 +1,5 @@
 export default ({ apollo, gql }) => class PublicationService {
-  getAll ({ projections, variables } = { projections: 'id appartment { id mainImg appartmentType isFurnished location rent details rooms bathrooms bedrooms livingrooms storageroom kitchen garage keeper terrace garden ac pool householdsTotal groundLevel conditions { advancePayment energyCommission prepaidRentMonths } ownerInfos { civility firstname lastname address isAlive phone email } likes favorite } publisher { id user { userType } status firstname lastname civility phone email } date status isNew views', variables: {} }) {
+  getAll ({ projections, variables } = { projections: 'id appartment { id mainImg appartmentType isFurnished location rent details rooms bathrooms bedrooms livingrooms storageroom kitchen garage keeper terrace garden ac pool householdsTotal groundLevel conditions { advancePayment energyCommission prepaidRentMonths } ownerInfos { civility firstname lastname address isAlive phone email } likes favorite } publisher { id user { id userType } status firstname lastname civility phone email } date status isNew views', variables: {} }) {
     return apollo.query({ query: gql`query { publications { ${projections} } }`, variables })
   }
 
